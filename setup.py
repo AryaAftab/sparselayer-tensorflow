d21ddede1a07a9607c026549a371083c7ab306cc
Arya Aftab <arya.aftab@gmail.com>
2021-09-12 08:40:23 +0430

    Initial commit

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
+import os
+import codecs
 from setuptools import setup, find_packages
+
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(*parts):
+    with codecs.open(os.path.join(current_path, *parts), 'r', 'utf8') as reader:
+        return reader.read()
+
+
 
 setup(
   name = 'sparselayer-tensorflow',
@@ -6,6 +18,8 @@ setup(
   version = '0.0.1',
   license='MIT',
   description = 'Tensorflow 2.X implementation of Sparse Layer.',
+  long_description=read_file('README.md'),
+  long_description_content_type='text/markdown',
   author = 'Arya Aftab',
   author_email = 'arya.aftab@gmail.com',
   url = 'https://github.com/AryaAftab/sparselayer-tensorflow',
